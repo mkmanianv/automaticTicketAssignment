d31139f8076ebc1e95af3c0818e40b16c5ab9762
Muthu Krishna Manian V <mmanian@xactlycorp.com>
2021-02-11 21:51:46 +0530

    Added inverse transform

--- NLP_Ticket_Classification/main.py
+++ NLP_Ticket_Classification/main.py
@@ -1,25 +1,27 @@
-from flask import Flask,request,Response, jsonify
-from joblib import load
+from flask import Flask, request, jsonify
+
+import numpy as np
+import joblib
+from pickle import load
 
 from PreProcessing import PreProcessing
 
 application = Flask(__name__)
 
-clf = load("Automatic_Ticket_Assignment_Capstone_Project.joblib")
+clf = joblib.load("Automatic_Ticket_Assignment_Capstone_Project.joblib")
+le = load(open('classes.pkl', 'rb'))
 
 def readRequest(text):
-    label = 0
-    dictionary = {}
-    # label = pipeline.predict(text)[0]
     preprocessing = PreProcessing()
-    return preprocessing.data_clean(text)
-    # if label == 0:
-    #     dictionary.update({"Sentiment of tweet is": "Positive"})
-    #
-    # else:
-    #     dictionary.update({"Sentiment of tweet is": "Negative"})
-    #
-    # return dictionary
+    clean_text = preprocessing.data_clean(text)
+    predict_value = clf.predict_proba([clean_text])
+    maxValue=np.amax(predict_value)
+    argMax = np.argmax(predict_value)
+    if(maxValue>0.5):
+        return "Your ticket is assigned to: "+le.inverse_transform([argMax])[0]
+    else:
+        return "Your ticket is assigned to: OTHERS"
+
 
 @application.route('/ticketclassification', methods=['POST', 'GET'])
 def TicketClassification():
@@ -28,7 +30,5 @@ def TicketClassification():
     return jsonify(rt)
 
 
-
 if __name__ == '__main__':
-    application.run(host="0.0.0.0",port=9052,debug=False)
-
+    application.run(host="0.0.0.0", port=9052, debug=False)
